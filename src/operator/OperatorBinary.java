61ad7b5901f11276582d41473a1bc0e7dba5e7d7
nanaaulia <nanaaulia.app@gmail.com>
2021-04-22 22:23:29 +0800

    Operator binary assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //operator (-) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Nana Aulia";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.98;
+
+}
