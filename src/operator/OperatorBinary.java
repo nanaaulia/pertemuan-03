d0124d98d36dbefcae522a65933ca37188e28890
nanaaulia <nanaaulia.app@gmail.com>
2021-04-22 22:24:00 +0800

    Operator binary aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -10,4 +10,12 @@ public class OperatorBinary {
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.98;
 
+    public static void main(String[] args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
